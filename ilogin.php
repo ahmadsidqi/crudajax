3f10c25289cb254a598f7c05d6d114dcedc0eac0
John Smith <example@email.com>
2019-03-19 21:57:24 +0700

    sukses

---
+++ ilogin.php
@@ -0,0 +1,36 @@
+
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Nyekrip Form Login</title>
+	
+	<!-- Skrip CSS -->
+   <link rel="stylesheet" href="style.css"/>
+  
+  </head>	
+  <body>
+	<div class="container">
+		<div class="main">
+	      <form action="login.php?op=in" method="post">
+			<h2>Welcome</h2><hr/>		
+			
+			<label>Username :</label>
+			<input id="name" name="username" placeholder="username" type="text">
+			
+			<label>Password :</label>
+			<input id="password" name="password" placeholder="**********" type="password">
+			
+			<input type="submit" name="submit" id="submit" value="Login">
+		  </form>
+		</div>
+   </div>
+
+  </body>
+</html>
+
+
+
+
+
+
